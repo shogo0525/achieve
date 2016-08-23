73aca3f24dad48570d0c28e22000bdbe8f036e0e
Shogo Inamoto <shogo_inamoto@Shogo-no-MacBook-Air.local>
2016-08-23 09:56:52 +0900

    微調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  PERMISSIBLE_ATTRIBUTES = %i(name)
 
   PERMISSIBLE_ATTRIBUTES = %i(name avatar avatar_cache)
 
